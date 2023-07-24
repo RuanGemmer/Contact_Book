1292fd3ad79ca236df90d55d7df492f3e952f4f3
RuanGemmer <ruangemmer@live.com>
2023-07-24 18:37:10 -0300

    Created Contact/models and configurated contact/admin.py

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(max_length=250, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.id}- {self.first_name} {self.last_name}'  # type: ignore
