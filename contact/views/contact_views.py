ef0e708a15bf01f10522bd90ef53781a7296db79
RuanGemmer <ruangemmer@live.com>
2023-07-25 15:08:56 -0300

    Created pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
-        'site_title': 'All contacts - '
+        'page_obj': page_obj,
+        'site_title': 'All contacts - ',
     }
 
     return render(
@@ -33,9 +39,14 @@ def search(request):
         ) \
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
-        'site_title': 'Search - '
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value
     }
 
     return render(
