a163d7e4bd91a516485991312cee8beb83a517a3
RuanGemmer <ruangemmer@live.com>
2023-07-25 13:29:49 -0300

    Created contact/Index.html and config style.css

--- contact/admin.py
+++ contact/admin.py
@@ -10,7 +10,7 @@ class ContactAdmin(admin.ModelAdmin):
         'last_name',
         'email',
         'phone',
-        'created_date',
+        'show',
     )
     ordering = '-id',
     search_fields = (
@@ -20,6 +20,7 @@ class ContactAdmin(admin.ModelAdmin):
     )
     list_per_page = 10
     list_max_show_all = 200
+    list_editable = 'show',
     list_display_links = 'id', 'first_name'
 
 
