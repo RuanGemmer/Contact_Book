1ab6b8d8351efe5729d82447127e04e0c139ecf1
RuanGemmer <ruangemmer@live.com>
2023-07-26 18:54:54 -0300

    Configured update and delete contact permission only for its owner

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -18,7 +18,9 @@ def create(request):
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             messages.success(request, 'Contato registrado com sucesso')
             return redirect('contact:contact', contact_id=contact.id)
 
@@ -43,7 +45,11 @@ def create(request):
 
 @login_required(login_url='contact:login')
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact,
+        pk=contact_id,
+        show=True,
+        owner=request.user)
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
@@ -80,7 +86,11 @@ def update(request, contact_id):
 
 @login_required(login_url='contact:login')
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact,
+        pk=contact_id,
+        show=True,
+        owner=request.user)
     confirmation = request.POST.get('confirmation', 'no')
 
     if confirmation == 'yes':
